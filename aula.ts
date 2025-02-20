125b1b6812c7983356f72b321bda31d0bee670c0
Guilherme Seeverino <guilherme.severino08@gmail.com>
2025-02-20 11:32:44 -0300

    aula 1

---
+++ aula.ts
@@ -0,0 +1,17 @@
+let saudacao: string = `Boa tarde!!`;
+let passatempo1: string = `pescar`;
+let passatempo2: string = `jogar futebol`;
+let passatempo3: string = `estudar`;
+let idade: number = 15;
+let nome: string = `Guilherme`;
+let faculdade: string = `metalurgia`;
+
+
+console.log (`${saudacao} meu nome é ${nome}, tenho ${idade} anos, no meu tempo
+ livre gosto de ${passatempo1}, ${passatempo2} e ${passatempo3}. Tenho a intenção 
+de me formar em ${faculdade}`)
+
+
+
+
+
